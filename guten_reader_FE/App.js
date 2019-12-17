6a29347a7bd2192d9e1478234f2c1967d05765d5
Zoe <zoekharvey@gmail.com>
2019-12-16 22:06:43 -0700

    Add navigation with home and Reader pages

--- guten_reader_FE/App.js
+++ guten_reader_FE/App.js
@@ -1,33 +1,25 @@
 import React from 'react';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import {mockText} from './mock-data'
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import Reader from './Components/Reader/Reader.js';
+import HomeScreen from './Components/HomeScreen/HomeScreen.js'
+import 'react-native-gesture-handler'
 
-export default function App() {
-  return (
-    // Might need to change ScrollView to FlatView which renders on page view, not all at once
-    <ScrollView>
-      <View style={styles.container}>
-        <Text style={styles.mockText}>{mockText}</Text>
-      </View>
-    </ScrollView>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginLeft: '10%',
-    marginRight: '10%',
-    flex: 1,
-    borderColor: 'red',
-    borderWidth: 1
-  },
-
-  mockText: {
-    fontSize: 20,
-    marginTop: 40
-  }
+const MainNavigator = createStackNavigator({
+  HomeScreen: {screen: HomeScreen},
+  Reader: { screen: Reader }
 });
+
+const App = createAppContainer(MainNavigator);
+
+export default App;
+
+
+
+
+
+
+
+
