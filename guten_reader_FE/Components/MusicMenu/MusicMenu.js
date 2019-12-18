806406fff5828a304f7f6678faea350b2a076bb4
Zoe <zoekharvey@gmail.com>
2019-12-18 14:29:53 -0700

    Resize music menu component to fit bottom of page

--- guten_reader_FE/Components/MusicMenu/MusicMenu.js
+++ guten_reader_FE/Components/MusicMenu/MusicMenu.js
@@ -1,16 +1,43 @@
 import React, {Component} from 'react';
-import { View, Text, ScrollView, StyleSheet, Image } from 'react-native';
+import { View, TouchableOpacity, Image, StyleSheet } from 'react-native';
 import '../../assets/volume-on.png'
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
 
 export default class MusicMenu extends Component {
+  
   state = {isMuted: false}
+
+  toggleSound = () => {
+    this.state.isMuted ? this.setState({isMuted: false}) : this.setState({isMuted: true})
+  }
 
   render() {
     return(
-      <View>
-        {this.state.isMuted && <Image source={require('../../assets/volume-on.png')} />}
-        {!this.state.isMuted && <Image source={require('../../assets/mute.png')} />}
+      <View style={styles.container}>
+        <TouchableOpacity onPress={this.toggleSound}>
+          {this.state.isMuted === true && <Image style={styles.volume} source={require('../../assets/volume-on.png')} />}
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.toggleSound}>
+          {!this.state.isMuted && <Image style={styles.mute} source={require('../../assets/mute.png')} />}
+        </TouchableOpacity>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+  volume: {
+    height: 50,
+    resizeMode: 'contain'
+  },
+  mute: {
+    height: 50,
+    resizeMode: 'contain'
+  }
+
+});
