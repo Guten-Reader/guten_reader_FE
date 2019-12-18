3ec6fbbbcb4ac072b9f1186e99d0a62546f0f422
Quinne Farenwald <q.farenwald@gmail.com>
2019-12-17 17:09:02 -0700

    Create MenuLibrary component and link to Library

---
+++ guten_reader_FE/Components/MenuLibrary/MenuLibrary.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function MenuLibrary() {
+
+    return (
+      <View>
+        <Text>Library Menu</Text>
+      </View>
+    )
+
+}
