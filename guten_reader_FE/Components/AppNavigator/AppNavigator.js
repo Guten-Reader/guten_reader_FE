ddbf7d34fac9e0dfbd452eccb1ac00073d5f8218
Zoe <zoekharvey@gmail.com>
2019-12-18 13:47:13 -0700

    Add conditional rendering of sound icons

--- guten_reader_FE/Components/AppNavigator/AppNavigator.js
+++ guten_reader_FE/Components/AppNavigator/AppNavigator.js
@@ -19,3 +19,4 @@ export default class App extends React.Component {
     return <AppContainer />;
   }
 }
+
--- guten_reader_FE/Components/MusicMenu/MusicMenu.js
+++ guten_reader_FE/Components/MusicMenu/MusicMenu.js
@@ -3,10 +3,14 @@ import { View, Text, ScrollView, StyleSheet, Image } from 'react-native';
 import '../../assets/volume-on.png'
 
 export default class MusicMenu extends Component {
-
+  state = {isMuted: false}
 
   render() {
     return(
+      <View>
+        {this.state.isMuted && <Image source={require('../../assets/volume-on.png')} />}
+        {!this.state.isMuted && <Image source={require('../../assets/mute.png')} />}
+      </View>
     )
   }
 }
