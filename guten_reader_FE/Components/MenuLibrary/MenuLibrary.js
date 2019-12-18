93bece1e52e4b50fe962d01e7c29a78bd9f1e11b
Quinne Farenwald <q.farenwald@gmail.com>
2019-12-18 08:57:31 -0700

    Edit menu to toolbar style

--- guten_reader_FE/Components/MenuLibrary/MenuLibrary.js
+++ guten_reader_FE/Components/MenuLibrary/MenuLibrary.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { View, Button, StyleSheet } from 'react-native';
+import { Toolbar } from 'react-native-material-ui';
 
 export default function MenuLibrary(props) {
     return (
-      <View style={styles.menu}>
-        <Button title="SEARCH" onPress={() => this.props.navigation.navigate('Search')}></Button>
-      </View>
+      <View style={styles.toolbar}>
+      <Toolbar
+        searchable={{
+        autoFocus: true,
+        placeholder: 'Search',
+      }}
+    />
+    </View>
     )
 }
 
 const styles = StyleSheet.create({
-  menu: {
-    backgroundColor: '#000000',
+  toolbar: {
+    alignItems: 'center',
     width: '100%',
-    height: 75,
-    justifyContent: 'center',
+    height: 110
   }
 })
