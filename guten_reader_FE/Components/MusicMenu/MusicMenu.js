7bcf41f7b80f570e88b0c45f9afcd0ac0b651865
Zoe <zoekharvey@gmail.com>
2019-12-18 13:45:29 -0700

    Add music menu component boiler plate

---
+++ guten_reader_FE/Components/MusicMenu/MusicMenu.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import { View, Text, ScrollView, StyleSheet, Image } from 'react-native';
+import '../../assets/volume-on.png'
+
+export default class MusicMenu extends Component {
+
+
+  render() {
+    return(
+    )
+  }
+}
