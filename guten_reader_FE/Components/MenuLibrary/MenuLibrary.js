280296e6e6839b884f428f6bc3d7837a6a7aaadb
Quinne Farenwald <q.farenwald@gmail.com>
2019-12-19 20:17:26 -0700

    Build custom footer nav

--- guten_reader_FE/Components/MenuLibrary/MenuLibrary.js
+++ guten_reader_FE/Components/MenuLibrary/MenuLibrary.js
@@ -1,27 +1,25 @@
 import React from 'react';
-import { View, Button, StyleSheet } from 'react-native';
+import { View, Button, StyleSheet, Text } from 'react-native';
 import { Toolbar } from 'react-native-material-ui';
 
 export default function MenuLibrary(props) {
     return (
       <View style={styles.toolbar}>
-      <Toolbar
-        searchable={{
-        autoFocus: true,
-        placeholder: 'Search',
-      }}
-    />
-    </View>
+        <Text>SEARCH</Text>
+      </View>
     )
 }
 
 const styles = StyleSheet.create({
   toolbar: {
-    alignItems: 'center',
-    color:'#53E69B',
     position: 'absolute',
     bottom: 0,
     right: 0,
-    left: 0
+    left: 0,
+    backgroundColor: '#53E69B',
+    justifyContent: 'center',
+    height: 65,
+    width: '100%',
+    alignItems: 'center'
   }
 })
