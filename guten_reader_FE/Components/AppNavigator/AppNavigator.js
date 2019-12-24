af9cebf9a292e85e454a13a2ecc40ec6ea826116
Michael <mschneider247@gmail.com>
2019-12-23 17:07:26 -0700

    Fix additional merge conflicts in AppNavigator

--- guten_reader_FE/Components/AppNavigator/AppNavigator.js
+++ guten_reader_FE/Components/AppNavigator/AppNavigator.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import { createAppContainer } from 'react-navigation';
-import HomeScreen from '../HomeScreen/HomeScreen';
-<<<<<<< HEAD
 import Search from '../Search/Search'
-import Reader from '../Reader/Reader'
-=======
 import Reader from '../Reader/Reader';
 import Library from '../Library/Library';
->>>>>>> development
+
 
 const AppContainer = createAppContainer({
   Reader: Reader,
-<<<<<<< HEAD
   Search: Search,
-=======
   Library: Library,
->>>>>>> development
 },
 {
   initialRouteName: 'Library',
