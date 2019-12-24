551c08582ebf7d86aa818898e35084cc030e4bfe
Quinne Farenwald <q.farenwald@gmail.com>
2019-12-23 21:33:00 -0700

    Relink ListLibrary and Reader components

--- guten_reader_FE/Components/AppNavigator/AppNavigator.js
+++ guten_reader_FE/Components/AppNavigator/AppNavigator.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from '../HomeScreen/HomeScreen';
 import Reader from '../Reader/Reader';
 import Library from '../Library/Library';
