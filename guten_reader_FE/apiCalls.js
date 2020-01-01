049fc17789048f74981a8c2d03024d0589de1c44
Quinne Farenwald <q.farenwald@gmail.com>
2020-01-01 16:44:01 -0700

    Fetch books from BE and display

--- guten_reader_FE/apiCalls.js
+++ guten_reader_FE/apiCalls.js
@@ -1,3 +1,8 @@
-
-
-
+export const getBooks = async() => {
+  const response = await fetch('https://guten-server.herokuapp.com/api/v1/users/4/books')
+  try {
+    return response.json()
+  } catch {
+    throw Error(response.statusText)
+  }
+}
