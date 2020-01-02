8703f0852571fa9dc667bd7f32a031f26e58de52
Quinne Farenwald <q.farenwald@gmail.com>
2020-01-01 17:05:59 -0700

    Add api call to get book pages

--- guten_reader_FE/apiCalls.js
+++ guten_reader_FE/apiCalls.js
@@ -1,5 +1,14 @@
-export const getBooks = async() => {
-  const response = await fetch('https://guten-server.herokuapp.com/api/v1/users/4/books')
+export const getBooks = async(userId) => {
+  const response = await fetch(`https://guten-server.herokuapp.com/api/v1/users/${userId}/books`)
+  try {
+    return response.json()
+  } catch {
+    throw Error(response.statusText)
+  }
+}
+
+export const getBookText = async(userId, bookId) => {
+  const response = await fetch(`https://guten-server.herokuapp.com/api/v1/users/${userId}/books/${bookId}`)
   try {
     return response.json()
   } catch {
