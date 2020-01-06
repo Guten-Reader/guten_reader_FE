fba692d208612c85d159c5e71306ce3eacc0c249
Quinne Farenwald <q.farenwald@gmail.com>
2020-01-05 17:59:39 -0700

    Edit Search styling to move Download to right side

--- guten_reader_FE/Components/ListSearch/ListSearch.js
+++ guten_reader_FE/Components/ListSearch/ListSearch.js
@@ -4,8 +4,10 @@ import { StyleSheet, View, Button, Text } from 'react-native';
 const ListSearch = ({ book }) => {
   return (
     <View style={styles.container}>
-      <Text>{book.title}</Text>
-      <Text>{book.author}</Text>
+      <View style={styles.text}>
+        <Text style={styles.title}>{book.title}</Text>
+        <Text style={styles.author}>{book.author}</Text>
+      </View>
       <Button title="DOWNLOAD" style={styles.button}/>
     </View>
   )
@@ -13,10 +15,24 @@ const ListSearch = ({ book }) => {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    flexDirection: 'row',
+    marginBottom: 20,
+    justifyContent: 'space-between'
   },
-  text: {
+  title: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 20,
+    paddingTop: 10,
+    paddingBottom: 10,
+    width: 200
   },
-  button: {
+  author: {
+    color: 'white',
+    fontSize: 15,
+    marginBottom: 10,
+    width: 200
   }
 });
 
