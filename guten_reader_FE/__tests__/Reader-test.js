619325cfb839e8516475e319b66bb50a5f775fa3
Zoe <zoekharvey@gmail.com>
2020-01-06 14:32:05 -0700

    Add setup test file

--- guten_reader_FE/__tests__/Reader-test.js
+++
@@ -1,9 +0,0 @@
-import 'react-native';
-import React from 'react';
-import { shallow } from 'enzyme';
-import Reader from '../Components/Reader/Reader';
-import renderer from 'react-test-renderer';
-
-it('renders correctly, test using Jest', () => {
-  renderer.create(<Reader />);
-});
