9a2b242e8c11f26d410edace271c87efd874bc96
Michael <mschneider247@gmail.com>
2020-01-06 21:29:16 -0700

    POST user chosen book to library

--- guten_reader_FE/Components/ListSearch/ListSearch.js
+++ guten_reader_FE/Components/ListSearch/ListSearch.js
@@ -4,13 +4,23 @@ import { StyleSheet, View, Button, Text } from 'react-native';
 const ListSearch = ({ book }) => {
 
   const addBookToLibrary = async () => {
-    //guten_id, author, title
     let newBook = {
       guten_id: book.id,
       title: book.title,
       author: book.author
-    }
-    console.log("New BOOK! ", newBook)
+    };
+    let options = {
+      method: 'POST',
+      body: JSON.stringify(newBook),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+    console.log("options:: ", options)
+
+    let response = await fetch(`https://guten-server.herokuapp.com/api/v1/users/4/books`, options);
+    const data = await response.json();
+    console.log("response data ::", data)
   }
 
   return (
