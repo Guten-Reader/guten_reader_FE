ca535ec385fce036c23fed5472f3729b52365ddd
Michael <mschneider247@gmail.com>
2020-01-27 20:13:40 -0700

    Add initial Picker for genre to Settings

--- guten_reader_FE/src/Components/Settings/Settings.js
+++ guten_reader_FE/src/Components/Settings/Settings.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Picker } from 'react-native';
 
 class Settings extends Component {
   constructor() {
     super();
     this.state = {
       userSettings: {
-        musicGenre: 'clasical',
+        musicGenre: 'classical',
         font: 'default',
         fontSize: '20',
-        darkMode: false
+        darkMode: 'false'
       }
     }
   }
@@ -18,6 +18,20 @@ class Settings extends Component {
     return (
       <View>
         <Text>Settings Component</Text>
+        <Text>Genre: {this.state.userSettings.musicGenre}</Text>
+        <Text>font: {this.state.userSettings.font}</Text>
+        <Text>fontSize: {this.state.userSettings.fontSize}</Text>
+        <Text>darkMode: {this.state.userSettings.darkMode}</Text>
+        <Picker
+          selectedValue={this.state.language}
+          style={{height: 50, width: 100}}
+          onValueChange={(itemValue, itemIndex) =>
+            this.setState({language: itemValue})
+        }>
+        <Picker.Item label="Classical" value="classical" />
+        <Picker.Item label="Metal" value="metal" />
+        <Picker.Item label="Techno" value="techno" />
+        </Picker>
       </View>
     )
   }
