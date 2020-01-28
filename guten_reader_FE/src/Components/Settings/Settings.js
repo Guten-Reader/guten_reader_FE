1913379dd468f1b50914df78e0c1e1f038a628ef
Michael <mschneider247@gmail.com>
2020-01-27 19:40:05 -0700

    Initial setup for Settings component

---
+++ guten_reader_FE/src/Components/Settings/Settings.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class Settings extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userSettings: {
+        musicGenre: 'clasical',
+        font: 'default',
+        fontSize: '20',
+        darkMode: false
+      }
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Settings Component</Text>
+      </View>
+    )
+  }
+}
+
+export default Settings;
